14df0daeb8bf8e1264ab68d16e21ae772b74bc75
Alan Tavares de Melo <alantmelo@live.com>
2019-12-03 13:50:29 -0300

    fix  calc-digitos-posicoes error

--- BrasilHelper.php
+++ BrasilHelper.php
@@ -2,6 +2,7 @@
 namespace sururulab\BrasilHelper;
 
 use sururulab\BrasilHelper\lib\pierophp\InscricaoEstadual;
+use sururulab\BrasilHelper\lib\pierophp\Utils;
 
 /**
 * Brasil Helper é um projeto open source que empacota via pacote composer um poderoso helper de utilidades para quem desenvolve software para regionalização do Brasil.
@@ -226,41 +227,6 @@ class BrasilHelper
          * @return int Os dígitos enviados concatenados com o último dígito
          *
          */
-        if ( ! function_exists('calc_digitos_posicoes') ) {
-            function calc_digitos_posicoes( $digitos, $posicoes = 10, $soma_digitos = 0 ) {
-                // Faz a soma dos dígitos com a posição
-                // Ex. para 10 posições:
-                //   0    2    5    4    6    2    8    8   4
-                // x10   x9   x8   x7   x6   x5   x4   x3  x2
-                //   0 + 18 + 40 + 28 + 36 + 10 + 32 + 24 + 8 = 196
-                for ( $i = 0; $i < strlen( $digitos ); $i++  ) {
-                    $soma_digitos = $soma_digitos + ( $digitos[$i] * $posicoes );
-                    $posicoes--;
-                }
-
-                // Captura o resto da divisão entre $soma_digitos dividido por 11
-                // Ex.: 196 % 11 = 9
-                $soma_digitos = $soma_digitos % 11;
-
-                // Verifica se $soma_digitos é menor que 2
-                if ( $soma_digitos < 2 ) {
-                    // $soma_digitos agora será zero
-                    $soma_digitos = 0;
-                } else {
-                    // Se for maior que 2, o resultado é 11 menos $soma_digitos
-                    // Ex.: 11 - 9 = 2
-                    // Nosso dígito procurado é 2
-                    $soma_digitos = 11 - $soma_digitos;
-                }
-
-                // Concatena mais um dígito aos primeiro nove dígitos
-                // Ex.: 025462884 + 2 = 0254628842
-                $cpf = $digitos . $soma_digitos;
-
-                // Retorna
-                return $cpf;
-            }
-        }
 
         // Verifica se o CPF foi enviado
         if ( ! $cpf ) {
@@ -280,12 +246,11 @@ class BrasilHelper
         // Captura os 9 primeiros dígitos do CPF
         // Ex.: 02546288423 = 025462884
         $digitos = substr($cpf, 0, 9);
-
         // Faz o cálculo dos 9 primeiros dígitos do CPF para obter o primeiro dígito
-        $novo_cpf = calc_digitos_posicoes( $digitos );
+        $novo_cpf = Utils::calc_digitos_posicoes( $digitos );
 
         // Faz o cálculo dos 10 dígitos do CPF para obter o último dígito
-        $novo_cpf = calc_digitos_posicoes( $novo_cpf, 11 );
+        $novo_cpf = Utils::calc_digitos_posicoes( $novo_cpf, 11 );
 
         // Verifica se o novo CPF gerado é idêntico ao CPF enviado
         if ( $novo_cpf === $cpf ) {
---
+++ lib/pierophp/Utils.php
@@ -0,0 +1,40 @@
+<?php
+namespace sururulab\BrasilHelper\lib\pierophp;
+
+class Utils
+{
+    public static function calc_digitos_posicoes( $digitos, $posicoes = 10, $soma_digitos = 0 ) {
+        // Faz a soma dos dígitos com a posição
+        // Ex. para 10 posições:
+        //   0    2    5    4    6    2    8    8   4
+        // x10   x9   x8   x7   x6   x5   x4   x3  x2
+        //   0 + 18 + 40 + 28 + 36 + 10 + 32 + 24 + 8 = 196
+        for ( $i = 0; $i < strlen( $digitos ); $i++  ) {
+            $soma_digitos = $soma_digitos + ( $digitos[$i] * $posicoes );
+            $posicoes--;
+        }
+
+        // Captura o resto da divisão entre $soma_digitos dividido por 11
+        // Ex.: 196 % 11 = 9
+        $soma_digitos = $soma_digitos % 11;
+
+        // Verifica se $soma_digitos é menor que 2
+        if ( $soma_digitos < 2 ) {
+            // $soma_digitos agora será zero
+            $soma_digitos = 0;
+        } else {
+            // Se for maior que 2, o resultado é 11 menos $soma_digitos
+            // Ex.: 11 - 9 = 2
+            // Nosso dígito procurado é 2
+            $soma_digitos = 11 - $soma_digitos;
+        }
+
+        // Concatena mais um dígito aos primeiro nove dígitos
+        // Ex.: 025462884 + 2 = 0254628842
+        $cpf = $digitos . $soma_digitos;
+
+        // Retorna
+        return $cpf;
+    }
+}
+?>
